f4411115cbfcb491e1f9557ab716f968b8efe8f2
Jose luis San martin <joseluis@MacBook-Air-de-Jose.local>
2020-07-25 18:23:46 -0400

    cambios de funcionalidad

--- src/project/endpoints.py
+++ src/project/endpoints.py
@@ -8,10 +8,28 @@ from functools import wraps
 from project.models import Usuario
 from project import db, bcrypt
 from project.schemas import usuario_schema
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 
 
 blueprint = Blueprint("usuarios", __name__)#para generar las rutas
+
+def usuario_a_dict(usuario):#el diccionario
+    return {
+    "id": usuario.id,
+    "nombre": usuario.nombre,
+    "email": usuario.email,
+    "password": usuario.password,
+    "fecha": usuario.fecha,
+    "sistema_o": usuario.sistema_o,
+    "procesador": usuario.procesador,
+    "almacenamiento": usuario.almacenamiento,
+    "ram": usuario.ram,
+    "grafica": usuario.grafica,
+    "direct_x": usuario.direct_x,
+    "gusto": usuario.gusto}
+
 
 def check_token():
     authorization = request.headers.get('Authorization')
@@ -33,44 +51,14 @@ def check_token():
     except:
         return False
 
-
-def usuario_a_dict(usuario):#el diccionario
-    return {
-    "id": usuario.id,
-    "nombre": usuario.nombre,
-    "email": usuario.email,
-    "password": usuario.password,
-    "fecha": usuario.fecha,
-    "sistema_o": usuario.sistema_o,
-    "procesador": usuario.procesador,
-    "almacenamiento": usuario.almacenamiento,
-    "ram": usuario.ram,
-    "grafica": usuario.grafica,
-    "direct_x": usuario.direct_x,
-    "gusto": usuario.gusto}
-
-
-def check_token(): #autorizador
-    authorization = request.headers.get("Authorization")
-
-    if authorization is None:
-        return False
-
-    partes = authorization.split(" ")
-
-    if len(partes) != 2:
-        return False
-
-        if partes[0] != "Bearer":
-            return False
-
-    token = partes[1]
-
-    try:
-        return jwt.decode(token, current_app.config["SECRET"])
-    except:
-        return False
-
+def autenticar(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        check_response = check_token()
+        if check_response is False:
+            return 'Unauthorized', 401
+        return f(check_response, *args, **kwargs)
+    return wrapper
 
 
 #@blueprint.route("/inicio")#rutas
@@ -78,47 +66,17 @@ def index():
     return "<h1>Hola</h1>"
 
 
-@blueprint.route("/inicio", methods=["GET", "POST"])
-def inicio():
-
-    datos = request.get_json()
-
-    email =datos["email"]
-    password = datos["password"]
-
-    usuario = Usuario.query.filter_by(email = email).first()
-
-    if usuario is None:
-        return "Not found", 404
-
-    if bcrypt.check_password_hash(usuario.password, password) is False:
-        return "not found", 404
-
-    payload = {
-        "sub": usuario.id,
-        "iat": datetime.datetime.now()}
-
-    return jwt.encode(payload, "C5rl5s-m2r6l2s14", algorithm="HS256")#problemas con el current_app.config["SECRET"] en este sector. Se prefiere usar la clave directamente
-
-
 @blueprint.route("/registro", methods=["POST"])
 def register():
-    datos = request.get_json()
-
-    nombre = datos["nombre"]
-    password = datos["password"]
-    email = datos["email"]
-
-    usuario = Usuario(**datos)
+    usuario = usuario_schema.load(request.json)
 
     db.session.add(usuario)#generador de sesion
     db.session.commit()
 
-    return usuario_a_dict(usuario) , 201
+    return usuario_schema.dump(usuario), 201
 
 
-
-@blueprint.route("/perfilDeUsuario/<id>", methods=["GET"])
+@blueprint.route("/login<id>", methods=["GET"])
 def view(id):
     check_response = check_token()
 
@@ -190,14 +148,15 @@ def login():
 
     email = datos['email']
     password = datos['password']
+    nombre = datos['nombre']
 
-    usuario = Usuario.query.filter_by(email=email).first()
+    usuario = Usuario.query.filter_by(email = email, password=password).first()
 
     if usuario is None:
         return 'Not found', 404
 
     if bcrypt.check_password_hash(usuario.password, password) is False:
-        return 'Not found', 404
+        return "not found", 404
 
     payload = {
         'sub': usuario.id,
@@ -209,3 +168,11 @@ def login():
         payload,
         current_app.config['SECRET'],
         algorithm='HS256')
+
+
+@blueprint.route('/token', methods=['GET'])
+@autenticar
+def usuario(payload):
+    usuario = Usuario.query.get_or_404(payload['sub'])
+
+    return usuario_schema.dump(usuario), 200
